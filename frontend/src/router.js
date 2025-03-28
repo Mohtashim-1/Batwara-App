5b9e86a04662202ac2dfe5d669003547970a0a01
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-03-29 01:58:05 +0500

    chore: frontend setup and cleanup

---
+++ frontend/src/router.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { session } from './data/session'
+import { userResource } from '@/data/user'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/Dashboard.vue'),
+  },
+  // {
+  //   name: 'Login',
+  //   path: '/account/login',
+  //   component: () => import('@/pages/Login.vue'),
+  // },
+]
+
+let router = createRouter({
+  history: createWebHistory('/frontend'),
+  routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  let isLoggedIn = session?.isLoggedIn || false; // Ensure it is always defined
+
+  try {
+    await userResource.promise;
+  } catch (error) {
+    isLoggedIn = false;
+  }
+
+  if (!isLoggedIn) {
+    window.location.href = "/login?redirect-to=/frontend";
+  } else {
+    next(); // Ensure next() is called if user is logged in
+  }
+});
+
+
+// router.beforeEach(async (to, from, next) => {
+//   let isLoggedIn = session.isLoggedIn
+//   try {
+//     await userResource.promise
+//   } catch (error) {
+//     isLoggedIn = false
+//   }
+
+//   if(!isLoggedIn){
+//     window.location.href = "/login?redirect-to=/frontend"
+//   }
+
+  // if (to.name === 'Login' && isLoggedIn) {
+  //   next({ name: 'Home' })
+  // } else if (to.name !== 'Login' && !isLoggedIn) {
+  //   next({ name: 'Login' })
+  // } else {
+  //   next()
+  // }
+// })
+
+export default router
